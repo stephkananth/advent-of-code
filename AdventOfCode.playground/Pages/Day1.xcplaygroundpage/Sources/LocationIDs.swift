81fd1075a69748375e5623e445e802f4534162fe
Stephanie Ananth <steph.ananth@gmail.com>
2024-12-02 07:51:36 -0500

    cleanup inputable

--- AdventOfCode.playground/Pages/Day1.xcplaygroundpage/Sources/LocationIDs.swift
+++ AdventOfCode.playground/Pages/Day1.xcplaygroundpage/Sources/LocationIDs.swift
@@ -13,19 +13,15 @@ public struct LocationIDs: Inputable {
     public private(set) var right: [Int] = []
 
     public init(from file: String) {
-        do {
-            let rawText = try String(contentsOf: getURL(from: file), encoding: .utf8)
-            let regex = /(?:(\d+)(?:\s{3})(\d+)(?:\n?))/
+        let rawText = getRawText(from: file)
+        let regex = /(?:(\d+)(?:\s{3})(\d+)(?:\n?))/
 
-            rawText.matches(of: regex).forEach { match in
-                if let left = Int(match.1), let right = Int(match.2) {
-                    self.count += 1
-                    self.left.append(left)
-                    self.right.append(right)
-                }
+        rawText.matches(of: regex).forEach { match in
+            if let left = Int(match.1), let right = Int(match.2) {
+                self.count += 1
+                self.left.append(left)
+                self.right.append(right)
             }
-        } catch {
-            fatalError(error.localizedDescription)
         }
 
         left.sort()
--- AdventOfCode.playground/Sources/Inputable.swift
+++ AdventOfCode.playground/Sources/Inputable.swift
@@ -11,7 +11,15 @@ public protocol Inputable {
     init(from file: String)
 }
 
-public extension InputModel {
+public extension Inputable {
+    func getRawText(from file: String) -> String {
+        do {
+            return try String(contentsOf: getURL(from: file), encoding: .utf8)
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+    }
+
     func getURL(from file: String) -> URL {
         guard let url = Bundle.main.url(forResource: file, withExtension: ".txt") else {
             fatalError("Unable to get url for file: \(file)")
